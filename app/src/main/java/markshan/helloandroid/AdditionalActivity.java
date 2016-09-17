a2e2da3463a04e8922e21db92c86f559a3c1846a
shanmango <shanmarkshan@gmail.com>
2016-09-16 23:59:12 -0700

    test2

---
+++ app/src/main/java/markshan/helloandroid/AdditionalActivity.java
@@ -0,0 +1,13 @@
+package markshan.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdditionalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_additional);
+    }
+}
--- app/src/main/java/markshan/helloandroid/MainActivity.java
+++ app/src/main/java/markshan/helloandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package markshan.helloandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final static String EXTRA_HELLO_ANDROID = "markshan.helloandroid.HELLO_ANDRODID";
+    public final static String EXTRA_RATING = "markshan.helloandroid.RATING";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +24,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        TextView tv = (TextView) findViewById(R.id.textView);
+        Button button = (Button) findViewById(R.id.button);
+        CheckBox checkbox = (CheckBox) findViewById(R.id.checkBox);
+
+        tv.setText("Mark Shan");
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(),AdditionalActivity.class);
+                intent.putExtra(EXTRA_HELLO_ANDROID, "Hello Android");
+                intent.putExtra(EXTRA_RATING, 3);
+                startActivity(intent);
             }
         });
     }
